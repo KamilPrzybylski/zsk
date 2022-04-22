32fa47200628c70b0615c440aee01001436f46e1
KamilPrzybylski <90440270+KamilPrzybylski@users.noreply.github.com>
2022-04-22 11:23:23 +0200

    Update main.cpp

--- 20220422/main.cpp
+++ 20220422/main.cpp
@@ -3,9 +3,21 @@
 using namespace std;
 
 int main() {
+	setlocale(LC_ALL, "polish");
 	
 	ofstream File;
-	File.open("plik.txt");
-	
+	//File.open("plik.txt")
+//ścieżka bezwzględna	
+	File.open("C:\\Users\\student\\Desktop\\vhjuki\\plik.txt");
+	if(File.is_open()){
+		//cout<<"ok"<<endl;
+		cout<<"Imię i nazwisko"<<endl;
+		string iin;
+		getline(cin, iin);
+		File<<iin;
+	}
+	else{
+		cout<<"error"<<endl;
+	}
 	return 0;
 }
